39857d919e121a3c46e3b116fd57cacbfddfe8ae
Melbourne Baldove <melbournebaldove@gmail.com>
2025-09-21 23:43:15 +0800

    feat: add GitHub Actions deploy key for haeru

--- modules/shared/ssh-keys.nix
+++ modules/shared/ssh-keys.nix
@@ -4,5 +4,6 @@
   users.users.melbournebaldove.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvRFinX32oEn1D4pBUmAZdmk+LofsuMG9rpmv87U0at melbournebaldove@Turing.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuJUmdNbq46Ge0V0ZnU9dwspG7ge9CNEeBcNFZWSKVK melbournebaldove@einstein"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeQYsbkqQwg5G38Y8a0/wDS7zjfaYogk+NDzaCh62bz haeru-deploy@github-actions"
   ];
 }
