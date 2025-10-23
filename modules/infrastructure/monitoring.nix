096d85b64e72267645c50a9302583124f83414b6
Melbourne Baldove <melbournebaldove@gmail.com>
2025-10-23 12:31:58 +0800

    chore: remove blackbox exporter configs

--- modules/infrastructure/monitoring.nix
+++ modules/infrastructure/monitoring.nix
@@ -95,35 +95,6 @@ in
           targets = [ "localhost:9115" ];
         }];
       }
-      {
-        job_name = "blackbox-http";
-        static_configs = [{
-          targets = [
-            "https://crm.workwithnextdesk.com"
-            "https://cms.workwithnextdesk.com"
-            "https://wiki.workwithnextdesk.com"
-            "https://n8n.workwithnextdesk.com"
-          ];
-        }];
-        metrics_path = "/probe";
-        params = {
-          module = [ "http_2xx" ];
-        };
-        relabel_configs = [
-          {
-            source_labels = [ "__address__" ];
-            target_label = "__param_target";
-          }
-          {
-            source_labels = [ "__param_target" ];
-            target_label = "instance";
-          }
-          {
-            target_label = "__address__";
-            replacement = "localhost:9115";
-          }
-        ];
-      }
     ] ++ haeruPrometheusJobs;
     
     alertmanagers = [
